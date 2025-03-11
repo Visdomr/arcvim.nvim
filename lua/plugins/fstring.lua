2811bfa145acbb406221aa46f9839715e798dc80
visdomr <runingback33@gmail.com>
2025-03-11 14:55:47 -0700

    fixed fstring plugin error.

--- lua/plugins/fstring.lua
+++ lua/plugins/fstring.lua
@@ -1,16 +1,4 @@
 return {
-<<<<<<< HEAD
-  "roobert/f-string-toggle.nvim",
-  config = function()
-    require("f-string-toggle").setup({
-      key_binding = "<leader>f",
-      key_binding_desc = "Toggle f-string",
-      filetypes = { "python" },
-    })
-  end,
-}
-
-=======
 	{
 		"roobert/f-string-toggle.nvim",
 		config = function()
@@ -21,8 +9,4 @@ return {
 			})
 		end,
 	}
-
-
-
 }
->>>>>>> 19a900d75b98578149b80b98018d2bed38b24391
